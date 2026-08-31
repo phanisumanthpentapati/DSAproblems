e1c424539fdc5364c7655df8df60855b9d54db0a
phanisumanthpentapati <sumanthpentapati1@gmail.com>
2026-08-31 15:19:35 +0530

    feat(medium): add number-of-occurrence2259 solution [Java]

---
+++ gfg/medium/number-of-occurrence2259/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int n=arr.length,count = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == target)
+            {
+                count++;
+            }
+            
+        }
+        return count;
+    }
+}
