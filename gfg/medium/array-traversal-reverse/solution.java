32e96fe0315f742ee1f9d6d1b5210d84f529b965
phanisumanthpentapati <sumanthpentapati1@gmail.com>
2026-09-03 21:56:41 +0530

    feat(medium): add array-traversal-reverse solution [Java]

---
+++ gfg/medium/array-traversal-reverse/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void arrayTraversalReverse(int[] arr, int n) {
+        // Code here
+      int left = 0;
+      int right = n - 1;
+      while (left < right) {
+          int temp = arr[left];
+          arr[left] = arr[right];
+          arr[right] = temp;
+          left++;
+          right--;
+      }
+      for (int i = 0; i < n; i++) {
+          System.out.print(arr[i] + " ");
+      }
+    }
+}
