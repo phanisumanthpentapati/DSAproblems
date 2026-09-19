1829c5a80a0b88d33369aa496bcbf383c6f06eea
phanisumanthpentapati <sumanthpentapati1@gmail.com>
2026-09-19 17:01:00 +0530

    feat(basic): add find-the-odd-occurence4820 solution [Java]

---
+++ gfg/basic/find-the-odd-occurence4820/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    int getOddOccurrence(int[] arr) {
+
+        int ans = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            ans = ans ^ arr[i];
+        }
+
+        return ans;
+    }
+}
